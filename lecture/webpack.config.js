9a0897e66c9928941fff7f883a3a66a92051ded1
leeyoonseo <okayoon.lee@gmail.com>
2022-08-03 22:37:52 +0900

    끝말잇기 class함수

--- lecture/webpack.config.js
+++ lecture/webpack.config.js
@@ -18,7 +18,17 @@ module.exports = {
       test: /\.jsx?/,
       loader: 'babel-loader',
       options: {
-        presets: ['@babel/preset-env', '@babel/preset-react'],
+        presets: [
+          // plugin들의 모음이 preset (즉 수십개의 plugin들이 합쳐져 있다.)
+          // 해당하는 preset에 대한 설정을 하고 싶을 경우 아래와 같이 작성 
+          ['@babel/preset-env', {
+            targets: {
+              browsers: ['> 5% in KR', 'last 2 chrome versions'], // 브라우저 호환 관련 옵션을 줄 수도 있다.
+            },
+            debug: true, // 개발 용에서 debug
+          }],
+          '@babel/preset-react'
+        ], 
         plugins: ['@babel/plugin-proposal-class-properties'],
       },
     }]
