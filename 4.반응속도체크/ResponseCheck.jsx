047566b5c2a1ed018875fa1691c86268f6bad2a0
leeyoonseo <okayoon.lee@gmail.com>
2022-08-09 17:34:13 +0900

    settimeout 추가

--- 4.반응속도체크/ResponseCheck.jsx
+++ 4.반응속도체크/ResponseCheck.jsx
@@ -7,19 +7,50 @@ class ResponseCheck extends Component {
     result: [],
   };
 
+  timeout;
+  startTime;
+  endTime;
+
   onClickScreen = () => {
-    //
+    const { state, message, result } = this.state;
+    
+    if (state === 'waiting') {
+      this.setState({
+        state: 'ready',
+        message: '초록색이 되면 클릭하세요.',
+      });
+      this.timeout = setTimeout(() => {
+        this.setState({
+          state: 'now',
+          message: '지금 클릭',
+        });
+
+        this.startTime = new Date();
+      }, Math.floor(Math.random() * 1000 + 2000)); // 2-3초 랜덤
+    } else if (state === 'ready') {
+      clearTimeout(this.timeout);
+
+      this.setState({
+        state: 'waiting',
+        message: '너무 성급하시군요! 초록색이 된 후에 클릭하세요.',
+      });
+    } else if (state === 'now') {
+      this.endTime = new Date();
+      this.setState((prevState) => {
+        return {
+          state: 'waiting',
+          result: [...prevState.result, this.endTime - this.startTime],
+          message: '클릭해서 시작하세요.',
+        }
+      });
+    }
   };
 
   renderAverage = () => {
-    const { result } = this.state;
-    return result.lengt === 1
-      // jsx에서는 false, undefined, null로 태그없음을 나타낸다.
-      ? null
-      : (
-        <div>평균 시간: {this.state.result.reduce((a, c) => a + c) / this.state.result.length}ms</div>
-      );
-  };
+    return this.state.result.length === 0 ? null : (
+      <div>평균 시간: {this.state.result.reduce((a, c) => a + c) / this.state.result.length}ms</div>
+    )
+  }
 
   render() {
     const { state, message } = this.state;
@@ -32,8 +63,8 @@ class ResponseCheck extends Component {
         >
           {message}
         </div>
-        
-        {this.renderAverage}  
+
+        {this.renderAverage()}  
       </>
     );
   }
